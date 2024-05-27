07dad4471ab6b6f2e11eba734b6c4fe6f9b5cbbc
Kacper <kacper.smolinski@hotmail.com>
2024-05-27 15:22:15 +0200

    add: Get and Post database API

--- backend/server.js
+++ backend/server.js
@@ -2,26 +2,49 @@ const express = require('express');
 const path = require('path');
 const mysql = require('mysql2');
 require('dotenv').config();
+
+// Create Express app
 const app = express();
+const PORT = process.env.PORT || 5000;
 
+// Middleware to parse JSON bodies
+app.use(express.json());
 
-//Database connection
-const connection = mysql.createConnection({
+// Create a MySQL connection pool
+const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10, // Adjust based on your needs
+    queueLimit: 0
 });
 
-connection.connect((err) => {
-    if (err) {
-        console.error('Error connecting to the database:', err.stack);
-        return;
-    }
-    console.log('Connected to the database as id ' + connection.threadId);
-});
 
-connection.end();
+//APIs
+app.get('/api/data', (req, res) => {
+    pool.query('SELECT * FROM posts;', (error, results) => {
+        if (error) {
+            console.error('Error executing query:', error.stack);
+            return;
+        }
+        console.log('Got data!');
+        res.json(results);
+    });
+})
+
+app.post('/api/data', (req, res) => {
+    const { author, title, textContent, priority, postDate } = req.body;
+    const query = 'INSERT INTO posts (author, title, text_content, priority, post_date) VALUES (?, ?, ?, ?, ?)';
+    pool.query(query, [author, title, textContent, priority, postDate], (error, result) => {
+        if (error) {
+            return res.status(500).json({message: error.message});
+        } else {
+            res.status(201).json({ message: 'User added successfully', userId: result.insertId });
+        }
+    })
+})
 
 
 // Serve static files from the React app
@@ -37,8 +60,6 @@ app.get('*', (req, res) => {
 
 
 //App is listening
-const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
     console.log(`App is listening on http://localhost:${PORT}/`)
 })
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,22 +2,33 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const App = () => {
-  const [date, setDate] = useState('');
+  const [data, setData] = useState();
+
+  const dataToSend = {
+    author: 'Kacper',
+    title: 'Title one',
+    textContent: 'Lorem ipsum...',
+    priority: 1,
+    postDate: '2022-10-12'
+  }
 
   useEffect(() => {
-    axios.get('/api/hello')
-      .then(response => {
-        setDate(response.data.message);
-      })
-      .catch(error => {
-        console.error('There was an error making the request!', error);
-      });
+      const fetchData = async () =>{
+        try {
+          const {data: response} = await axios.post('/api/data', dataToSend);
+          setData(response.message);
+        } catch (error) {
+          console.error('There was an error making the request!', error);
+        }
+      }
+  
+      fetchData();
   }, []);
 
   return (
     <>
       <div>App</div>
-      <div>{date}</div>
+      <div>{data}</div>
     </>
   )
 }
