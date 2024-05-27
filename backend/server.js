4324c95a20cef064b8ffa4b3df262ddf0c1cba7b
Kacper <kacper.smolinski@hotmail.com>
2024-05-27 13:51:28 +0200

    add: Database connection

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,28 @@
 const express = require('express');
 const path = require('path');
-const cors = require('cors');
+const mysql = require('mysql2');
+require('dotenv').config();
 const app = express();
 
-app.use(cors());
 
-app.get('/api/hello', (req, res) => {
-    const currentDate = new Date();
-    const formattedTime = currentDate.toLocaleTimeString();
-    res.json({ message: formattedTime});
+//Database connection
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database:', err.stack);
+        return;
+    }
+    console.log('Connected to the database as id ' + connection.threadId);
+});
+
+connection.end();
+
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'public')));
@@ -22,6 +35,8 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', '404.html'));
   });
 
+
+//App is listening
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
